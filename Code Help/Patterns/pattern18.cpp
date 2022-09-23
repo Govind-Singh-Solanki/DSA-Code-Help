b638dd03fa461a5f4f9bf3f3a8b8fc8134ca1117
Govind <gssolanki30@gmail.com>
2022-09-23 10:59:39 +0530

    Patterns in DSA

---
+++ Code Help/Patterns/pattern18.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout<<"Enter the number of rows : ";
+    cin>>n;
+
+    int row = 1;
+
+    while(row<=n) {
+
+        char start = 'A' + n -row;
+        int col = 1;
+        while(col<=row) {
+            cout<<start<<" ";
+            start++;
+            col += 1;
+        }
+        start++;
+        cout<<endl;
+        row += 1;
+    }
+    return 0;
+}
