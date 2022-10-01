6b342aae0ae9e5f1f8d34cdcb1eeefb003dc80a4
Govind <gssolanki30@gmail.com>
2022-10-01 12:35:36 +0530

    Patterns DSA

---
+++ HomeWork/Patterns/pattern20.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"Enter number of rows : ";
+    cin>>n;
+
+    int row = 1;
+
+    while(row <= n)
+    {
+        int col = n - row + 1;
+        while(col >= 1)
+        {
+            cout<<"*";
+            col--;
+        }
+        cout<<endl;
+        row++;
+    }
+
+    return 0;
+}
